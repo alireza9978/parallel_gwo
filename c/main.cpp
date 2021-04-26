7de74a817a9e47aa7d6e3dffc17383447a31e609
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-04-26 13:57:01 +0430

    update main to run all algorithms

--- c/main.cpp
+++ c/main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 typedef double Fn(double *, int);
 
 int main()
-{   
+{
     int repeat_algorithm_count = 10;
     GrayWolfTwo temp_two;
-    omp_set_num_threads(8);
+    omp_set_num_threads(2);
     int dimention = 10000;
     int searchAgentNumber = 100;
     int iteraton = 10;
@@ -27,28 +27,36 @@ int main()
     }
 
     unsigned long noraml_mean_time = 0, paralllel_for_objective_mean_time = 0, parallel_for_algorithm_mean_time = 0,
-     prallel_for_both_mean_time = 0, parallel_objective_mean_time = 0, parallel_algorithm_mean_time = 0,
-     parallel_both_mean_time = 0;
-    for (int i=0; i<repeat_algorithm_count; i++){
-        cout << "----------normal-----------" << endl;
-        Fn *f = &Eval::rastrigin;
+                  prallel_for_both_mean_time = 0, parallel_objective_mean_time = 0, parallel_algorithm_mean_time = 0,
+                  parallel_both_mean_time = 0;
+    for (int i = 0; i < repeat_algorithm_count; i++)
+    {
+
         Solution so = temp_two.solve(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
-        so.print();
         noraml_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
-        cout << endl;
-        cout << "----------parallel_for_objective-----------" << endl;
+
         so = temp_two.solve_normal_parallel_for_objective(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
-        so.print();
         paralllel_for_objective_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
-        cout << endl;
-        cout << "----------parallel_objective-----------" << endl;
+
         so = temp_two.solve_normal_parallel_objective(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
-        so.print();
         parallel_objective_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
-        cout << endl;    
+
+        so = temp_two.solve_parallel(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
+        parallel_algorithm_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
+
+        so = temp_two.solve_parallel_both(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
+        parallel_both_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
+
+        so = temp_two.solve_parallel_for(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
+        parallel_for_algorithm_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
+
+        so = temp_two.solve_parallel_for_both(lowerBand, upperBand, dimention, searchAgentNumber, iteraton, false);
+        prallel_for_both_mean_time += (so.endTime - so.startTime) / repeat_algorithm_count;
     }
-    printf("normal_mean_time : %d ms \nparallel_for_objective_mean_time : %d ms \n
-    parallel_objective_mean_time : %d ms \n",
-    noraml_mean_time, paralllel_for_objective_mean_time, parallel_objective_mean_time);
+    printf("noraml_mean_time : %ld ms \nparalllel_for_objective_mean_time : %ld ms \nparallel_for_algorithm_mean_time : %ld ms \nprallel_for_both_mean_time : %ld ms \nparallel_objective_mean_time : %ld ms \nparallel_algorithm_mean_time : %ld ms \nparallel_both_mean_time : %ld ms \n",
+           noraml_mean_time, paralllel_for_objective_mean_time, parallel_for_algorithm_mean_time,
+           prallel_for_both_mean_time, parallel_objective_mean_time, parallel_algorithm_mean_time,
+           parallel_both_mean_time);
+
     return 0;
 }
